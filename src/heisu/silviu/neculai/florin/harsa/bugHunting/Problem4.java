2ff7c54b418cef5d00ba8c2a1758aca1b0b29f83
Heisu Silviu-Neculai <SilviuNeculai.Heisu@solera.com>
2022-11-22 11:35:36 +0100

    Problem 4 final

--- src/heisu/silviu/neculai/florin/harsa/bugHunting/Problem4.java
+++ src/heisu/silviu/neculai/florin/harsa/bugHunting/Problem4.java
@@ -1,5 +1,19 @@
 package heisu.silviu.neculai.florin.harsa.bugHunting;
 
 public class Problem4 {
-
+	
+public String getStrippedSubString() {
+	int sstart= start, ssend=end;
+	String c="";
+	while(sstart<ssend) {
+	c+= input.charAt(sstart);
+	
+		if(c.charAt(sstart)==' ' || c.charAt(sstart)=='\n' || c.charAt(sstart)=='\r' || c.charAt(sstart)=='\t') {
+			break;
+		}
+		++sstart;
+	}
+	return c;
+	
+}
 }
